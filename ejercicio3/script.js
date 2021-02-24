f994815a03aabcc57082babb996e5ef9d46045a5
jmmas123 <josemaria.mas10a@gmail.com>
2021-02-23 20:40:17 -0600

    ff4

--- ejercicio3/script.js
+++ ejercicio3/script.js
@@ -5,6 +5,15 @@ function Sum() {
     document.getElementsByName("sum")[0].value = sum;
 
     alert(sum)
-
-
 }
+
+function addToTable() {
+    var table = document.getElementById("data");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2)
+    cell1.innerHTML = "num1";
+    cell2.innerHTML = "num2";
+    cell3.innerHTML = "sum"
+  }
