2c3bccafdf8c70cbafe361f26162b737ebffe59e
jmmas123 <josemaria.mas10a@gmail.com>
2021-02-23 20:11:39 -0600

    ff

---
+++ ejercicio3/script.js
@@ -0,0 +1,10 @@
+function Sum() {
+    var num1 = document.getElementsByName("num1")[0].value;
+    var num2 = document.getElementsByName("num2")[0].value;
+    var sum = Number(num1) + Number(num2);
+    document.getElementsByName("sum")[0].value = sum;
+
+    alert(sum)
+
+
+}
